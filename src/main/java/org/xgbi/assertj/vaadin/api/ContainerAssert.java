708d157647d9c1048ecbb6346216187f59145182
Vincent Demeester <vincent+git@demeester.fr>
2013-10-01 18:00:45 +0200

    Prepare for new assert

--- src/main/java/org/xgbi/assertj/vaadin/api/ContainerAssert.java
+++ src/main/java/org/xgbi/assertj/vaadin/api/ContainerAssert.java
@@ -30,6 +30,25 @@ public class ContainerAssert extends AbstractAssert<ContainerAssert, Container> 
 	}
 
 	/**
+	 * Assert that the {@link Container} is not empty, a.k.a. has at least one
+	 * item (not checking the propertyIds)..
+	 * 
+	 * @return the {@link ContainerAssert} for assertions chaining.
+	 * 
+	 * @throws AssertionError
+	 *             if the actual {@link Container} is ``null``.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} is empty.
+	 */
+	public ContainerAssert isNotEmpty() {
+		isNotNull();
+		if (getActual().size() == 0) {
+			failWithMessage("Expected to have a least one item.");
+		}
+		return this;
+	}
+
+	/**
 	 * Assert that the {@link Container} has no propertyIds.
 	 * 
 	 * @return the {@link ContainerAssert} for assertions chaining.
@@ -61,7 +80,8 @@ public class ContainerAssert extends AbstractAssert<ContainerAssert, Container> 
 	 *            {@link Container}.
 	 * @return this {@link ContainerAssert} for assertions chaining.
 	 * 
-	 * @throws IllegalArgumentException if the given propertyId is {@code null}.
+	 * @throws IllegalArgumentException
+	 *             if the given propertyId is {@code null}.
 	 * @throws AssertionError
 	 *             if the actual {@link Container} is {@code null}.
 	 * @throws AssertionError
@@ -73,7 +93,8 @@ public class ContainerAssert extends AbstractAssert<ContainerAssert, Container> 
 	 */
 	public ContainerAssert hasPropertyId(Object propertyId) {
 		isNotNull();
-		throwIllegalArgumentExceptionIfTrue(propertyId == null, "The propertyId to look for should not be null");
+		throwIllegalArgumentExceptionIfTrue(propertyId == null,
+				"The propertyId to look for should not be null");
 		Collection<?> actualPropertyIds = getActual().getContainerPropertyIds();
 		isNotNull(actualPropertyIds);
 		if (!actualPropertyIds.contains(propertyId)) {
@@ -92,7 +113,8 @@ public class ContainerAssert extends AbstractAssert<ContainerAssert, Container> 
 	 *            {@link Container}.
 	 * @return this {@link ContainerAssert} for assertions chaining.
 	 * 
-	 * @throws IllegalArgumentException if the given propertyIds is {@code null}.
+	 * @throws IllegalArgumentException
+	 *             if the given propertyIds is {@code null}.
 	 * @throws AssertionError
 	 *             if the actual {@link Container} is {@code null}.
 	 * @throws AssertionError
@@ -104,11 +126,149 @@ public class ContainerAssert extends AbstractAssert<ContainerAssert, Container> 
 	 */
 	public ContainerAssert hasPropertyIds(Object... propertyIds) {
 		isNotNull();
-		throwIllegalArgumentExceptionIfTrue(propertyIds == null, "The propertyIds to look for should not be null");
-		throwIllegalArgumentExceptionIfTrue(propertyIds.length == 0, "The propertyIds to look for should not be empty");
+		throwIllegalArgumentExceptionIfTrue(propertyIds == null,
+				"The propertyIds to look for should not be null");
+		throwIllegalArgumentExceptionIfTrue(propertyIds.length == 0,
+				"The propertyIds to look for should not be empty");
 		for (Object propertyId : propertyIds) {
 			hasPropertyId(propertyId);
 		}
+		return this;
+	}
+
+	/**
+	 * Assert that the {@link Container} has not the given propertyId.
+	 * 
+	 * @param propertyId
+	 *            the propertyId (Object) to look for in the actual
+	 *            {@link Container}.
+	 * @return this {@link ContainerAssert} for assertions chaining.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             if the given propertyId is {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} is {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container}'s propertyIds collection is
+	 *             {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} does contains the given
+	 *             propertyId.
+	 */
+	public ContainerAssert hasNotPropertyId(Object propertyId) {
+		isNotNull();
+		throwIllegalArgumentExceptionIfTrue(propertyId == null,
+				"The propertyId to look for should not be null");
+		Collection<?> actualPropertyIds = getActual().getContainerPropertyIds();
+		isNotNull(actualPropertyIds);
+		if (actualPropertyIds.contains(propertyId)) {
+			failWithMessage(
+					"Expected to contain the propertyId [%s], but did not.",
+					propertyId);
+		}
+		return this;
+	}
+
+	/**
+	 * Assert that the {@link Container} has not the given propertyIds
+	 * (several).
+	 * 
+	 * @param propertyIds
+	 *            the propertyIds (Object) to look for in the actual
+	 *            {@link Container}.
+	 * @return this {@link ContainerAssert} for assertions chaining.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             if the given propertyIds is {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} is {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container}'s propertyIds collection is
+	 *             {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} does contains one of the
+	 *             given propertyId.
+	 */
+	public ContainerAssert hasNotPropertyIds(Object... propertyIds) {
+		isNotNull();
+		throwIllegalArgumentExceptionIfTrue(propertyIds == null,
+				"The propertyIds to look for should not be null");
+		throwIllegalArgumentExceptionIfTrue(propertyIds.length == 0,
+				"The propertyIds to look for should not be empty");
+		for (Object propertyId : propertyIds) {
+			hasNotPropertyId(propertyId);
+		}
+		return this;
+	}
+
+	/**
+	 * Assert that the {@link Container} has the given itemId.
+	 * 
+	 * @param itemId
+	 *            the itemId (Object) to look for in the actual
+	 *            {@link Container}.
+	 * @return this {@link ContainerAssert} for assertions chaining.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             if the given itemId is {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} is {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} does not contains the given
+	 *             itemId.
+	 */
+	public ContainerAssert hasItemId(Object itemId) {
+		isNotNull();
+		throwIllegalArgumentExceptionIfTrue(itemId == null,
+				"The itemId to look for should not be null");
+		if (!getActual().containsId(itemId)) {
+			failWithMessage(
+					"Expected to contain the itemId [%s], but did not.",
+					itemId);
+		}
+		return this;
+	}
+
+	/**
+	 * Assert that the {@link Container} has not the given itemId.
+	 * 
+	 * @param itemId
+	 *            the itemId (Object) to look for in the actual
+	 *            {@link Container}.
+	 * @return this {@link ContainerAssert} for assertions chaining.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             if the given itemId is {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} is {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} does contains the given
+	 *             itemId.
+	 */
+	public ContainerAssert hasNotItemId(Object itemId) {
+		isNotNull();
+		throwIllegalArgumentExceptionIfTrue(itemId == null,
+				"The itemId to look for should not be null");
+		if (getActual().containsId(itemId)) {
+			failWithMessage(
+					"Expected to not contain the itemId [%s], but did.",
+					itemId);
+		}
+		return this;
+	}
+	
+	public ContainerAssert hasProperty(Object propertyId, Object itemId) {
+		// FIXME implementation
+		return this;
+	}
+
+	public ContainerAssert hasTypeFor(Object propertyId, Class<?> type) {
+		// FIXME implementation
+		return this;
+	}
+
+	public ContainerAssert hasSize(int size) {
+		// FIXME implementation
 		return this;
 	}
 
