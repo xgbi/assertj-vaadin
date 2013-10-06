e6cbafec407c8db333f4e9507b7b5e565c445ad6
Vincent Demeester <vincent+git@demeester.fr>
2013-10-06 11:08:50 +0200

    Update some assertions

--- src/main/java/org/xgbi/assertj/vaadin/api/container/ContainerAssert.java
+++ src/main/java/org/xgbi/assertj/vaadin/api/container/ContainerAssert.java
@@ -5,19 +5,20 @@ import java.util.Collection;
 import org.xgbi.assertj.vaadin.api.AbstractAssert;
 
 import com.vaadin.data.Container;
+import com.vaadin.data.Property;
 
 /**
  * Assertions for Vaadin {@link Container}.
- *  
+ * 
  * @author Vincent Demeester
- *
+ * 
  */
 public class ContainerAssert extends AbstractAssert<ContainerAssert, Container> {
 
 	public ContainerAssert(Container actual) {
 		super(actual, ContainerAssert.class);
 	}
-	
+
 	protected ContainerAssert(Container actual, Class<?> type) {
 		super(actual, type);
 	}
@@ -235,8 +236,7 @@ public class ContainerAssert extends AbstractAssert<ContainerAssert, Container> 
 				"The itemId to look for should not be null");
 		if (!getActual().containsId(itemId)) {
 			failWithMessage(
-					"Expected to contain the itemId [%s], but did not.",
-					itemId);
+					"Expected to contain the itemId [%s], but did not.", itemId);
 		}
 		return this;
 	}
@@ -263,23 +263,83 @@ public class ContainerAssert extends AbstractAssert<ContainerAssert, Container> 
 				"The itemId to look for should not be null");
 		if (getActual().containsId(itemId)) {
 			failWithMessage(
-					"Expected to not contain the itemId [%s], but did.",
-					itemId);
+					"Expected to not contain the itemId [%s], but did.", itemId);
 		}
 		return this;
 	}
-	
+
+	/**
+	 * Assert that the {@link Container} has the given {@link Property}
+	 * identified by the given itemId and propertyId.
+	 * 
+	 * @param itemId
+	 *            the itemId (Object) to look for in the actual
+	 *            {@link Container}.
+	 * @param propertyId
+	 *            the propertyId (Object) to look for in the actual
+	 *            {@link Container}.
+	 * @return this {@link ContainerAssert} for assertions chaining.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             if the given itemId is {@code null}.
+	 * @throws IllegalArgumentException
+	 *             if the given propertyId is {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} is {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} does contains the
+	 *             {@link Property} identified by the given itemId and
+	 *             propertyId.
+	 */
 	public ContainerAssert hasProperty(Object propertyId, Object itemId) {
 		isNotNull();
 		throwIllegalArgumentExceptionIfTrue(propertyId == null,
 				"The propertyId to look for should not be null");
 		throwIllegalArgumentExceptionIfTrue(itemId == null,
 				"The itemId to look for should not be null");
+		Property property = getActual()
+				.getContainerProperty(itemId, propertyId);
+		if (property == null) {
+			failWithMessage(
+					"Expected to contain a property identified by itemId [%s] and propertyId [%s]",
+					itemId, propertyId);
+		}
 		return this;
 	}
 
-	public ContainerAssert hasTypeFor(Object propertyId, Class<?> type) {
-		// FIXME implementation
+	/**
+	 * Assert that the {@link Container} has the given type for the
+	 * {@link Property} identified by the given propertyId.
+	 * 
+	 * @param propertyId
+	 *            the propertyId (Object) to look for in the actual
+	 *            {@link Container}.
+	 * @param expectedType
+	 *            the type to look for in the actual {@link Container}.
+	 * @return this {@link ContainerAssert} for assertions chaining.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             if the given propertyId is {@code null}.
+	 * @throws IllegalArgumentException
+	 *             if the given type is {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} is {@code null}.
+	 * @throws AssertionError
+	 *             if the actual {@link Container} does not have the given type
+	 *             for the property identified by the given propertyId.
+	 */
+	public ContainerAssert hasTypeFor(Object propertyId, Class<?> expectedType) {
+		isNotNull();
+		throwIllegalArgumentExceptionIfTrue(propertyId == null,
+				"The propertyId to look for should not be null");
+		throwIllegalArgumentExceptionIfTrue(expectedType == null,
+				"The type to look for should not be null");
+		Class<?> actualType = getActual().getType(propertyId);
+		if (!expectedType.equals(actualType)) {
+			failWithMessage(
+					"Expected property identified by [%s] to have the type [%s], but had [%s]",
+					propertyId, expectedType, actualType);
+		}
 		return this;
 	}
 
@@ -287,12 +347,12 @@ public class ContainerAssert extends AbstractAssert<ContainerAssert, Container> 
 		// FIXME implementation
 		return this;
 	}
-	
+
 	public ContainerAssert equals(Container container) {
 		// FIXME implementation
 		return this;
 	}
-	
+
 	public ContainerAssert same(Container container) {
 		return this;
 	}
